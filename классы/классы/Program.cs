89e713daa7d6f255e619fd01069d31396a325f44
dmitriy2k20 <118421220+dmitriy2k20@users.noreply.github.com>
2022-11-18 22:22:26 +0900

    Add files via upload

---
+++ классы/классы/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace классы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                Users user = new Users()
+                {
+                    Name = "Иван",
+                    Surname = "Иванов",
+                    Login = "123",
+                    Password = "123",
+                };
+                Console.WriteLine(user.Name);
+                Console.WriteLine("Введите имя");
+                user.Name = Console.ReadLine();
+                Console.WriteLine($"Ваше новое имя - {user.Name}");
+                Console.ReadLine();
+            }
+        }
+
+
+        class Users
+        {
+            public string Name;
+            public string Surname;
+
+            public string Login;
+            public string Password;
+        }
+    }
+}
---
+++ классы/классы/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace классы
+{
+    class User
+    {
+        public string Name;
+        public string Surname;
+
+        public string Login;
+        public string Password;
+    }
+}
